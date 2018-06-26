0a574685dc4b97129074f9e57318c5d7d2f99b4a
Aman09Agarwal <40361108+Aman09Agarwal@users.noreply.github.com>
2018-06-26 22:23:53 +0530

    Day8 work

---
+++ zomatoplus/src/main/java/com/thinkxfactor/zomatoplus/models/Restaurant.java
@@ -0,0 +1,123 @@
+package com.thinkxfactor.zomatoplus.models;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+
+@Entity
+@Table(name="tbl_restaurant")
+public class Restaurant implements Serializable{
+	
+	
+	
+	public Restaurant() {
+		super();
+	}
+
+	public Restaurant(Long id, String city, String name, String contact, String address, String description,
+			String website, Integer likeCount) {
+		super();
+		this.id = id;
+		this.city = city;
+		this.name = name;
+		this.contact = contact;
+		this.address = address;
+		this.description = description;
+		this.website = website;
+		this.likeCount = likeCount;
+	}
+
+	@Id
+	@GeneratedValue //auto-increment
+	@Column(name="restaurant_id")
+	private Long id;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="contact")
+	private String contact;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="website")
+	private String website;
+	
+	@Column(name="like_count")
+	private Integer likeCount;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public Integer getLikeCount() {
+		return likeCount;
+	}
+
+	public void setLikeCount(Integer likeCount) {
+		this.likeCount = likeCount;
+	}
+}
